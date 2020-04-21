64620945e82c0807f9b652373d364357c9e027fe
Hansel <hanselcapillo@hotmail.com>
2020-04-21 15:25:38 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'subir',
+    loadChildren: () => import('./subir/subir.module').then( m => m.SubirPageModule)
+  },
 ];
 
 @NgModule({
