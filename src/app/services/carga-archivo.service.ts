fe2bbda927934cc018a1e4560d0d697fd826c033
Hansel <hanselcapillo@hotmail.com>
2020-04-25 18:43:42 -0500

    correccion de advertencia en consola

--- src/app/services/carga-archivo.service.ts
+++ src/app/services/carga-archivo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
 import { ToastController } from '@ionic/angular';
 import { map } from 'rxjs/internal/operators';
 
